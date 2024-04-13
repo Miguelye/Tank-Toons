43191d245cf973909a249cbfa327779f9acd632b
Miguel Yepes <miguelyegarcia94@gmail.com>
2024-04-13 17:51:34 -0600

    Pre-Regeneration

--- Source/ToonTanks/Private/Tank.cpp
+++ Source/ToonTanks/Private/Tank.cpp
@@ -4,13 +4,38 @@
 #include "Tank.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
 
 ATank::ATank()
 {
-	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm Component"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
 	SpringArm->SetupAttachment(RootComponent);
 
-	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera Component"));
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	CameraComp->SetupAttachment(SpringArm);
+}
 
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+	Super::SetupPlayerInputComponent(PlayerInputComponent); //Call the parent version
+
+	// Get the player controller
+	auto playerController = Cast<APlayerController>(GetController());
+
+	// Get the local player enhanced input subsystem
+	auto eiSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerController->GetLocalPlayer());
+	//Add the input mapping context
+	eiSubsystem->AddMappingContext(inputMapping, 0);
+
+	// Get the EnhancedInputComponent
+	auto playerEIcomponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	//Bind Move() to the mapping
+	//BindAction for enhanced system takes Action, ETriggerEvent, object, and function
+	//ETriggerEvent is an enum, where Triggered means "button is held down".
+	playerEIcomponent->BindAction(inputMoveForward, ETriggerEvent::Triggered, this, &ATank::Move);
 }
--- Source/ToonTanks/Public/Tank.h
+++ Source/ToonTanks/Public/Tank.h
@@ -17,6 +17,7 @@ class TOONTANKS_API ATank : public ABasePawn
 	GENERATED_BODY()
 public:
 	ATank();
+	void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent);
 private:
 	//UPROPERTY(This are Macros most of them giving different levels of Access to variables, check notes for more info)
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
@@ -24,5 +25,15 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* CameraComp;
 
-
+	//Setting up inputs. Mapping context and Input Action variables
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+		class UInputMappingContext* inputMapping;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+		class UInputAction* inputMove;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+		class UInputAction* inputTurn;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+		class UInputAction* inputAim;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+		class UInputAction* inputFire;
 };
