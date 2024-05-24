323ec401fea4e1395f414d0e29eb0f5ee8693184
Miguel Yepes <miguelyegarcia94@gmail.com>
2024-05-24 07:18:02 -0600

    Created a Health Component, inside created a callback function DamageTaken and bounded it to the OnTakeAnyDamage delegate.

---
+++ Source/ToonTanks/Private/HealthComponent.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Health = MaxHealth;
+
+	//Binds the DamageTaken function of this UHealthComponent instance to the OnTakeAnyDamage event of the owning actor
+	//this ensures that whenever the owning actor takes any damage, the DamageTaken Function will be called,
+	//allowing this component to handle the damage event appropriately.
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::DamageTaken);
+	
+}
+
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UHealthComponent::DamageTaken(AActor* DamagedActor, float Damage, const UDamageType* DamageType, class AController* Instigator, AActor* DamageCauser)
+{
+
+}
