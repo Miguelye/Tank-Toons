7b537447a153a908cf212091abba5f12fdbc5756
Miguel Yepes <miguelyegarcia94@gmail.com>
2024-05-24 08:28:25 -0600

    Created the GameMode Class, Set this as the default GameMode, Set the Default Pawn

---
+++ Source/ToonTanks/Public/TankToonGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankToonGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATankToonGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
